8605c7ecdd0e6340b34fc452560ff4139b554eb1
Jason RG <jasonrodriguezempleo@gmail.com>
2025-06-19 17:04:30 +0200

    Initial commit

---
+++ test_add.py
@@ -0,0 +1,10 @@
+from add import add
+
+def test_add():
+    assert add(2, 3) == 5
+    # assert add(-1, 1) == 10 # descomentar para tener resultados
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(10, 20) == 30
+
+# test_add() # solo para pruebas
