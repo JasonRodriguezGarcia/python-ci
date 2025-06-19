216e73a9441f118d6231c91980a436af1e7550d2
Jason RG <jasonrodriguezempleo@gmail.com>
2025-06-19 18:01:22 +0200

    Cambio test_add.py para que falle

--- test_add.py
+++ test_add.py
@@ -4,7 +4,7 @@ def test_add():
     assert add(2, 3) == 5
     # assert add(-1, 1) == 10 # descomentar para tener resultados
     assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+    assert add(0, 0) == 10
     assert add(10, 20) == 30
 
 # test_add() # solo para pruebas
